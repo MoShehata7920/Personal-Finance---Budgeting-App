76c62f5a2f9d1929d153249b1c26df0b4ca677d8
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2025-03-18 12:11:09 +0200

    home Screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+import 'package:personal_finance/manager/budget_cubit/budget_cubit.dart';
+import 'package:personal_finance/manager/budget_cubit/budget_state.dart';
 import 'package:personal_finance/manager/user/user_cubit.dart';
-
+import 'package:personal_finance/models/budget_model.dart';
 import 'package:personal_finance/resources/icons_manager.dart';
 import 'package:personal_finance/resources/strings_manager.dart';
 
@@ -15,52 +18,89 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("${AppStrings.welcome} ${userCubit.state.name} "),
+        title: Text("${AppStrings.welcome} ${userCubit.state.name}"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            _buildBalanceCard(),
-            const SizedBox(height: 20),
-            _buildSpendingChart(),
-            const SizedBox(height: 20),
-            _buildSectionTitle(AppStrings.recentTransactions),
-            _buildTransactionList(),
-            const SizedBox(height: 20),
-            _buildSectionTitle(AppStrings.budgetOverview),
-            _buildBudgetProgress(),
-          ],
-        ),
+      body: BlocBuilder<BudgetCubit, BudgetState>(
+        builder: (context, state) {
+          if (state is BudgetLoaded) {
+            final budget = state.budget;
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView(
+                children: [
+                  _buildBalanceCard(budget),
+                  const SizedBox(height: 20),
+                  _buildSpendingChart(budget),
+                  const SizedBox(height: 20),
+                  _buildSectionTitle(AppStrings.recentTransactions),
+                  _buildTransactionList(budget),
+                  const SizedBox(height: 20),
+                  _buildSectionTitle(AppStrings.budgetOverview),
+                  _buildBudgetProgress(budget),
+                ],
+              ),
+            );
+          } else if (state is BudgetError) {
+            return Center(child: Text(state.message));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
 
-  Widget _buildBalanceCard() {
+  Widget _buildBalanceCard(BudgetModel budget) {
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       elevation: 4,
       color: Colors.red,
-      child: const Padding(
-        padding: EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(AppStrings.totalBalance,
+            const Text(AppStrings.totalBalance,
                 style: TextStyle(color: Colors.white, fontSize: 18)),
-            SizedBox(height: 8),
-            Text("\$5,250.00",
-                style: TextStyle(
+            const SizedBox(height: 8),
+            Text("\$${budget.totalBudget.toStringAsFixed(2)}",
+                style: const TextStyle(
                     color: Colors.white,
                     fontSize: 28,
                     fontWeight: FontWeight.bold)),
+            const SizedBox(height: 8),
+            Text(
+                "${AppStrings.spent} \$${budget.totalSpent.toStringAsFixed(2)}",
+                style: const TextStyle(
+                    color: Colors.white70,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500)),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildSpendingChart() {
+  Widget _buildSpendingChart(BudgetModel budget) {
+    List<String> last7Days = List.generate(7, (index) {
+      return DateFormat('EEE')
+          .format(DateTime.now().subtract(Duration(days: 6 - index)));
+    });
+
+    Map<String, double> dailySpending = {for (var day in last7Days) day: 0.0};
+
+    for (var transaction in budget.transactions) {
+      String day = DateFormat('EEE').format(transaction.date);
+      if (dailySpending.containsKey(day)) {
+        dailySpending[day] = (dailySpending[day] ?? 0) + transaction.amount;
+      }
+    }
+
+    List<FlSpot> spendingData = [];
+    for (int i = 0; i < last7Days.length; i++) {
+      spendingData.add(FlSpot(i.toDouble(), dailySpending[last7Days[i]]!));
+    }
+
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       elevation: 4,
@@ -92,24 +132,11 @@ class HomeScreen extends StatelessWidget {
                       sideTitles: SideTitles(
                         showTitles: true,
                         getTitlesWidget: (value, meta) {
-                          switch (value.toInt()) {
-                            case 0:
-                              return const Text(AppStrings.sat);
-                            case 1:
-                              return const Text(AppStrings.sun);
-                            case 2:
-                              return const Text(AppStrings.mon);
-                            case 3:
-                              return const Text(AppStrings.tue);
-                            case 4:
-                              return const Text(AppStrings.wed);
-                            case 5:
-                              return const Text(AppStrings.thu);
-                            case 6:
-                              return const Text(AppStrings.fri);
-                            default:
-                              return const Text("");
+                          int index = value.toInt();
+                          if (index >= 0 && index < last7Days.length) {
+                            return Text(last7Days[index]);
                           }
+                          return const Text("");
                         },
                         reservedSize: 30,
                       ),
@@ -118,15 +145,7 @@ class HomeScreen extends StatelessWidget {
                   borderData: FlBorderData(show: false),
                   lineBarsData: [
                     LineChartBarData(
-                      spots: [
-                        const FlSpot(0, 50),
-                        const FlSpot(1, 80),
-                        const FlSpot(2, 40),
-                        const FlSpot(3, 90),
-                        const FlSpot(4, 30),
-                        const FlSpot(5, 100),
-                        const FlSpot(6, 60),
-                      ],
+                      spots: spendingData,
                       isCurved: true,
                       color: Colors.red,
                       barWidth: 3,
@@ -144,56 +163,57 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
+  Widget _buildTransactionList(BudgetModel budget) {
+    return Column(
+      children: budget.transactions.take(3).map((transaction) {
+        return Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          elevation: 2,
+          child: ListTile(
+            leading: const Icon(AppIcons.shoppingCart, color: Colors.red),
+            title: Text(transaction.categoryName),
+            subtitle: Text(transaction.date.toLocal().toString().split(' ')[0]),
+            trailing: Text(
+              "- \$${transaction.amount.toStringAsFixed(2)}",
+              style: const TextStyle(
+                  color: Colors.red, fontWeight: FontWeight.bold),
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  Widget _buildBudgetProgress(BudgetModel budget) {
+    return Column(
+      children: budget.categories.map((category) {
+        double progress = budget.totalBudget > 0
+            ? category.spentAmount / budget.totalBudget
+            : 0;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(category.name,
+                style: const TextStyle(fontWeight: FontWeight.bold)),
+            const SizedBox(height: 4),
+            LinearProgressIndicator(
+              value: progress.clamp(0.0, 1.0),
+              backgroundColor: Colors.grey[300],
+              color: progress >= 1.0 ? Colors.red : Colors.green,
+            ),
+            const SizedBox(height: 10),
+          ],
+        );
+      }).toList(),
+    );
+  }
+
   Widget _buildSectionTitle(String title) {
     return Text(
       title,
       style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-    );
-  }
-
-  Widget _buildTransactionList() {
-    return Column(
-      children: List.generate(3, (index) => _buildTransactionItem(index)),
-    );
-  }
-
-  Widget _buildTransactionItem(int index) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      elevation: 2,
-      child: ListTile(
-        leading: const Icon(AppIcons.shoppingCart, color: Colors.red),
-        title: Text("${AppStrings.transactions} ${index + 1}"),
-        subtitle: const Text("02 March 2025"),
-        trailing: const Text("- \$50.00",
-            style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
-      ),
-    );
-  }
-
-  Widget _buildBudgetProgress() {
-    return Column(
-      children: [
-        _buildBudgetItem("Food", 70),
-        _buildBudgetItem("Transport", 40),
-        _buildBudgetItem("Entertainment", 90),
-      ],
-    );
-  }
-
-  Widget _buildBudgetItem(String category, double progress) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(category, style: const TextStyle(fontWeight: FontWeight.bold)),
-        const SizedBox(height: 4),
-        LinearProgressIndicator(
-          value: progress / 100,
-          backgroundColor: Colors.grey[300],
-          color: progress > 80 ? Colors.red : Colors.green,
-        ),
-        const SizedBox(height: 10),
-      ],
     );
   }
 }
